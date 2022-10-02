4628dae5c8fdbf942a812ec0615f2948521b413f
Jaydan Urwin <jaydan@jaydanurwin.com>
2022-10-01 23:51:03 -0600

    added lit support and dark theme toggle

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,15 @@ import { defineConfig } from 'astro/config';
 import { settings } from './src/data/settings';
 import sitemap from "@astrojs/sitemap";
 
+import lit from "@astrojs/lit";
 
 // https://astro.build/config
 export default defineConfig({
   site: settings.site,
-  integrations: [sitemap()],
+  integrations: [sitemap(), lit()],
   vite: {
     ssr: {
-      external: ["svgo"],
-    },
-  },
+      external: ["svgo"]
+    }
+  }
 });
---
+++ src/components/ThemeToggleButton/ThemeToggleButton.ts
@@ -0,0 +1,87 @@
+
+
+import { html, css, LitElement } from "lit";
+import { customElement, property } from "lit/decorators.js";
+import { lightThemeIcon, darkThemeIcon } from "./icons";
+
+export const tagName = "theme-toggle-button"
+
+@customElement("theme-toggle-button")
+export class ThemeToggleButton extends LitElement {
+  static styles = css`
+    :host {
+      display: inline-block;
+    }
+    button {
+      width: 28px;
+      height: 28px;
+      padding: 0.25rem;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      position: relative;
+      box-sizing: border-box;
+      border: none;
+      outline: none;
+      background-color: var(--theme-primary-light);
+      fill: currentcolor;
+      color: inherit;
+      text-decoration: none;
+      cursor: pointer;
+      vertical-align: middle;
+      -webkit-tap-highlight-color: transparent;
+      border-radius: 50%;
+      border: 1px solid var(--theme-primary);
+      transition: background-color 100ms cubic-bezier(0.6, -0.28, 0.735, 0.045);
+    }
+  `;
+
+  // set the _doc element
+  private _doc = document.firstElementChild;
+
+  /**
+   * The theme for the website the component is being used on
+   */
+  @property({ type: String, reflect: true, attribute: "theme" })
+  theme = "light";  
+
+  firstUpdated() {
+    this._getCurrentTheme();
+  }
+
+  private _getCurrentTheme() {
+		// check for a local storage theme first
+		const localStorageTheme = localStorage.getItem('theme');
+		if (localStorageTheme !== null) {
+			this._setTheme(localStorageTheme);
+		} else if (this._doc!.hasAttribute("color-scheme")) {
+      this._setTheme(this._doc!.getAttribute("color-scheme")!);
+    } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      this._setTheme("dark");
+    }
+	}
+
+	private _setTheme(theme) {
+		this._doc.setAttribute('color-scheme', theme);
+    this.theme = theme;
+  }
+
+  private _toggleTheme() {
+    if (this.theme === "dark") {
+      this._setTheme("light");
+    } else {
+      this._setTheme("dark");
+    }
+  }
+
+  render() {
+    return html`<button
+      @click=${this._toggleTheme}
+      title=${`Enable ${this.theme === "dark" ? "Light" : "Dark"} Theme`}
+    >
+      ${this.theme === "dark"
+        ? html`${lightThemeIcon}`
+        : html`${darkThemeIcon}`}
+    </button>`;
+  }
+}
