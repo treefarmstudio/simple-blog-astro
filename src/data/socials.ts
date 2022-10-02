fd20b3375ecca76db5c22b52b1a0221ef686b6ec
Jaydan Urwin <jaydan@jaydanurwin.com>
2022-10-01 22:43:10 -0600

    social list

---
+++ src/data/socials.ts
@@ -0,0 +1,17 @@
+export const socials = [
+  {
+    name: 'Twitter',
+    url: 'https://twitter.com/littlesticksdev',
+    icon: 'mdi:twitter'
+  },
+  {
+    name: 'GitHub',
+    url: 'https://github.com/littlesticks',
+    icon: 'mdi:github'
+  },
+  {
+    name: 'Instagram',
+    url: 'https://instagram.com',
+    icon: 'mdi:instagram'
+  }
+]
