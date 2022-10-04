b7a3acb69afeafc71941271898ab65429712b087
Jaydan Urwin <jaydan@jaydanurwin.com>
2022-10-03 20:52:32 -0600

    blog categories

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -6,3 +6,26 @@ export function formatDate(date) {
     day: 'numeric'
   });
 }
+
+export function slugify(string) {
+  return string
+    .toString()
+    .trim()
+    .toLowerCase()
+    .replace(/\s+/g, '-')
+    .replace(/[^\w\-]+/g, '')
+    .replace(/\-\-+/g, '-')
+    .replace(/^-+/, '')
+    .replace(/-+$/, '');
+}
+
+export function generateCategoryData(categories) {
+  let categoryData = [];
+  categories.forEach((category) => {
+    categoryData.push({
+      name: category,
+      slug: `${slugify(category)}`,
+    });
+  });
+  return categoryData;
+}
