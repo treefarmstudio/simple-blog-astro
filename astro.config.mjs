edaef7a96b401f725d16b60d4fecb6761b78c835
Jaydan Urwin <jaydan@jaydanurwin.com>
2022-10-03 20:34:25 -0600

    using theme-toggle-button as client script

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from 'astro/config';
 import { settings } from './src/data/settings';
 import sitemap from "@astrojs/sitemap";
-import lit from "@astrojs/lit";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: settings.site,
-  integrations: [sitemap(), lit(), mdx()],
+  integrations: [sitemap(), mdx()],
   vite: {
     ssr: {
       external: ["svgo"]
