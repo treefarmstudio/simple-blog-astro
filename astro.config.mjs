734209070613839f3197409e59f441f8b9d11f3d
Jaydan Urwin <jaydan@jaydanurwin.com>
2022-10-03 20:06:26 -0600

    mdx support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from 'astro/config';
 import { settings } from './src/data/settings';
 import sitemap from "@astrojs/sitemap";
-
 import lit from "@astrojs/lit";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: settings.site,
-  integrations: [sitemap(), lit()],
+  integrations: [sitemap(), lit(), mdx()],
   vite: {
     ssr: {
       external: ["svgo"]
